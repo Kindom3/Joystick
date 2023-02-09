ca54dd7442cb17dedb5ed90719f2168da9871bc3
TEVIN LEWIS <TML22D@FSU.EDU>
2023-02-09 01:35:13 -0500

    changed pin 3 to 8 in robs code in order to match previous

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,40 @@
 #include <Arduino.h>
+// above line is required for PlatformIO
 
-const int SW_pin = 8; // digital pin8 connected to switch output
-const int X_pin = 0; // analog pin connected to X output
-const int Y_pin = 1; // analog pin connected to Y output
+// connect a joystick to the Arduino:
+// pins A0 & A1 are connected to the X & Y pots of the joystick
+// pin 8 is connected to the button/switch pin of the joystick
+
+const int buttonPin = 3;
+
 void setup() {
-  pinMode(SW_pin, INPUT);
-  digitalWrite(SW_pin, HIGH);
   Serial.begin(9600);
+
+  pinMode(buttonPin, INPUT_PULLUP);
+  
+  while (Serial.available() <= 0) {
+    Serial.println("hello"); // send a "ready" message
+    delay(300);              // every 1/3 second
+  }
 }
+
 void loop() {
-  Serial.print("Switch:  ");
-  Serial.print(digitalRead(SW_pin));
-  Serial.print(" | ");
-  Serial.print("X-axis: ");
-  Serial.print(analogRead(X_pin));
-  Serial.print(" | ");
-  Serial.print("Y-axis: ");
-  Serial.print(analogRead(Y_pin));
-  Serial.println(" | ");
-  delay(200);
+  if (Serial.available() > 0) {
+    // read (and discard) the incoming byte:
+    int inByte = Serial.read();
+
+    // joystick X value
+    int sensorValue = analogRead(A0);
+    Serial.print(sensorValue);
+    Serial.print(",");
+
+    // joystick Y value
+    sensorValue = analogRead(A1);
+    Serial.print(sensorValue);
+    Serial.print(",");
+
+    // joystick button
+    sensorValue = digitalRead(buttonPin);
+    Serial.println(sensorValue);
+  }
 }
